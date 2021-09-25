6debf8f1c870c53f72d1db15c80b8ca4e9a85034
hwd-2020 <26042380@qq.com>
2021-09-26 00:39:15 +0800

    deploy

---
+++ assets/404.49ae5436.js
@@ -0,0 +1,1 @@
+import{r as o,o as e,a,e as n,w as r,h as t}from"./vendor.b3a2989e.js";const s={},d={id:"not-found-page"},u=t(" 404 "),l=t("返回首页");s.render=function(t,s){const c=o("el-button");return e(),a("div",d,[u,n(c,{onClick:s[0]||(s[0]=o=>t.$router.replace("/logon"))},{default:r((()=>[l])),_:1})])};export{s as default};
